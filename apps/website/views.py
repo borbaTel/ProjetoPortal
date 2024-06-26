11d317af9c5bbe8ee2868255bbe50dc48218fcc8
borbaTel <gabriel.bribeiro@telefonica.com>
2024-06-26 16:06:27 -0300

    navbar

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+#@login_required
 def index(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
         return redirect('login')
 
-    return render(request, 'website_template/index.html')
+    return render(request, 'website_template/index.html', {'user':request.user})
